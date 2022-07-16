7ae3d1247053a4e4ae0eee990d109508a947d8f9
milad heidari <milad.heidari.dev@gmail.com>
2022-07-16 13:14:15 +0430

    change banner btn text

--- components/Banner.js
+++ components/Banner.js
@@ -12,7 +12,7 @@ function Banner() {
 
       <div className='absolute top-1/2 w-full text-center' >
         <p className='text-sm sm:text-lg'>Not sure where to go? Perfect.</p>
-        <button className='text-purple-500 bg-white px-10 py-4 shadow-md cursor-pointer rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>i'm flexible</button>
+        <button className='text-purple-500 bg-white px-10 py-4 shadow-md cursor-pointer rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>i am flexible</button>
       </div>
     </div>
   )
