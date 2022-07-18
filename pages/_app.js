4e3c03d62e3ed77942c8e395d1e6036711639128
milad heidari <milad.heidari.dev@gmail.com>
2022-07-18 13:53:00 +0430

    progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
-
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from 'next/router'
 function MyApp({ Component, pageProps }) {
+  const progress = new ProgressBar({
+    size:4 ,
+    color: "#FE595E",
+    className:'z-50',
+    delay:100
+  });
+
+  Router.events.on('routeChangeStart',progress.start)
+  Router.events.on('routeChangeComplete',progress.finish)
+  Router.events.on('routeChangeError',progress.finish)
+
+
   return <Component {...pageProps} />
 }
 
